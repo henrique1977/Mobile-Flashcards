2a43b0e51cbd354c9734ac53dacb6cb7073a11ef
Henrique <henrique@paylease.com>
2018-10-03 06:08:34 -0700

    feat: add react natigation and icons
    Add react-navigation and a bottom tab.
    
    Add Icons that can be used throughout the whole app.

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from 'react-navigation';
+
+const Decks = () => (
+  <View style={styles.container}>
+    <MaterialCommunityIcons name="cards" color="grey" size={100} />
+    <Text>List of Decks</Text>
+  </View>
+);
+
+const AddDeck = () => (
+  <View style={styles.container}>
+    <MaterialCommunityIcons name="note-plus" color="grey" size={100} />
+    <Text>Add a new Deck</Text>
+  </View>
+);
+
+const Tabs = createBottomTabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: "Decks",
+      tabBarIcon: <MaterialCommunityIcons name="cards" color="grey" size={25} />
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: "Add Deck",
+      tabBarIcon: <MaterialCommunityIcons name="note-plus" color="grey" size={25} />
+    }
+  },
+//}
+// ,
+// {
+//   tabBarOptions: {
+//   activeTintColor: '#e91e63',
+//   labelStyle: {
+//     fontSize: 14,
+//   },
+//   style: {
+//     backgroundColor: 'grey',
+//   },
+// }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Tabs />
     );
   }
 }
