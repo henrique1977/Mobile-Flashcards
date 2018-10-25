f8e902ce6776aa7199865b6f14a12c8139eed400
Henrique <henrique@paylease.com>
2018-10-25 10:22:00 -0700

    feat: add redux store; stack navigator
    Add redux store
    
    Create a stack navigator for the app screens/pages

--- App.js
+++ App.js
@@ -1,51 +1,65 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native';
+
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
-/**
-@TODO:
-  add redux
-  add redux-connect
-  create tests
-  create store
-  create reducers
-  create actions to:
-    - add new deck
-    - remove deck
-    - add new card to deck
-    - name the deck
-    - shuffle deck function
+/** Redux **/
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
+import reducer from './redux/reducers/index';
+import {decksMdl} from './redux/middleware/DecksMiddleware';
+import {notificationsMdl} from './redux/middleware/NotificationsMiddleware';
+import { appHasStarted } from './redux/actions/Control';
+
+/** pages **/
+import AddDeck from './pages/AddDeck';
+import Decks from './pages/Decks';
+import DeckDetail from './pages/DeckDetail';
+import AddCard from './pages/AddCard';
+import Quiz from './pages/Quiz';
 
 
-*/
+const store = createStore(reducer, applyMiddleware(...decksMdl, ...notificationsMdl, logger));
+store.dispatch(appHasStarted());
 
-const FlexDemo = props => (
-  <View style={{flex: 1}}>
-    <View style={{flex: 1, backgroundColor: 'red'}} />
-    <View style={{flex: 2, backgroundColor: 'green'}} />
-    <View style={{flex: 3, backgroundColor: 'blue'}} />
-  </View>
-);
+//AsyncStorage.setItem('DECKS_STORAGE_KEY', JSON.stringify({}));
+//AsyncStorage.removeItem('DECKS_STORAGE_KEY');
+//AsyncStorage.removeItem('NOTIFICATION_KEY');
 
-const Decks = () => (
-  <View style={styles.container}>
-    <MaterialCommunityIcons name="cards" color="grey" size={100} />
-    <Text>List of Decks</Text>
-    <FlexDemo />
-  </View>
-);
 
-const AddDeck = () => (
-  <View style={styles.container}>
-    <MaterialCommunityIcons name="note-plus" color="grey" size={100} />
-    <Text>Add a new Deck</Text>
-  </View>
-);
-
-const Tabs = createBottomTabNavigator({
+const Stack = createStackNavigator({
   Decks: {
     screen: Decks,
+    navigationOptions: {
+      title: 'Decks'
+    }
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      title: 'Deck'
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      title: 'Add a Card'
+    }
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      title: 'Quiz'
+    }
+  }
+});
+
+const Tabs = createBottomTabNavigator({
+  Stack: {
+    screen: Stack,
     navigationOptions: {
       tabBarLabel: "Decks",
       tabBarIcon: <MaterialCommunityIcons name="cards" color="grey" size={25} />
@@ -57,32 +71,16 @@ const Tabs = createBottomTabNavigator({
       tabBarLabel: "Add Deck",
       tabBarIcon: <MaterialCommunityIcons name="note-plus" color="grey" size={25} />
     }
-  },
-  Demo: {
-    screen: FlexDemo,
-    navigationOptions: {
-      tabBarLabel: "Demo",
-      tabBarIcon: <MaterialCommunityIcons name="note-plus" color="grey" size={25} />
-    }
-  },
-//}
-// ,
-// {
-//   tabBarOptions: {
-//   activeTintColor: '#e91e63',
-//   labelStyle: {
-//     fontSize: 14,
-//   },
-//   style: {
-//     backgroundColor: 'grey',
-//   },
-// }
+  }
 });
 
 export default class App extends React.Component {
+
   render() {
     return (
+      <Provider store={store}>
         <Tabs />
+      </Provider>
     );
   }
 }
